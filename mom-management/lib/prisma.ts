1879704efc38967a76f98d888b0f8baa9e0ff083
Nency Parmar <nencyparmar@Nencys-MacBook-Air.local>
2026-03-19 10:36:59 +0530

    Generated Solved...

--- mom-management/lib/prisma.ts
+++ mom-management/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
 const adapter = new PrismaMariaDb({
     host: "localhost",
     port: 3306,
